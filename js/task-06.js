5798817ec084c879777069fbf5761b944bc80bcf
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2020-11-29 12:34:05 +0200

    Made refactoring

--- js/task-06.js
+++ js/task-06.js
@@ -6,7 +6,7 @@ while (true) {
   if (input === null) {
     alert(`Общая сумма чисел равна ${total}`);
     break;
-  } else if (isNaN(input)) {
+  } else if (Number.isNaN(Number(input))) {
     alert('Было введено не число, попробуйте еще раз');
   } else {
     total += Number(input);
